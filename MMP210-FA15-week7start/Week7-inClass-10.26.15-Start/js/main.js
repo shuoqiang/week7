0a7ad191a84c19d218902bba4ee9547a31ed5fec
Shuo Qiang <shuo.qiang@f1006-09-34181.bmcc.cuny.edu>
2015-10-26 14:37:38 -0400

    setup

---
+++ MMP210-FA15-week7start/Week7-inClass-10.26.15-Start/js/main.js
@@ -0,0 +1,9 @@
+//global variables
+
+function setup(){
+    //create the canvas. put into a variable to associate with a element
+    var cnv = createCanvas(800,3000);
+    cnv.parent("windows");
+
+}//end setup
+
