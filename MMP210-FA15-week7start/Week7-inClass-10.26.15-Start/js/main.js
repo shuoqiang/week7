c98d4a4ce62e451f2be5585f20297ce083f6be0f
Shuo Qiang <shuo.qiang@f1006-09-34181.bmcc.cuny.edu>
2015-10-26 15:08:07 -0400

    add loop to array

--- MMP210-FA15-week7start/Week7-inClass-10.26.15-Start/js/main.js
+++ MMP210-FA15-week7start/Week7-inClass-10.26.15-Start/js/main.js
@@ -5,5 +5,25 @@ function setup(){
     var cnv = createCanvas(800,3000);
     cnv.parent("windows");
 
+    //test:var myArray = ["hello",3,"bye"];
+    var myArray=[];//create blank array
+
+    //add stuff to the array with a loop
+    for(var i=0; i<1000;i++){
+        //myArray[i] = "item"+i;
+        myArray[i]=i;
+    }
+    //loop to go through every item in the array
+    for(var i=0;i<myArray.length;i++){
+        myArray[i]+=10;
+        //console.log(myArray[i]);
+    }
+        console.log(myArray);
+
+    console.log(myArray[0]);
+    console.log(myArray[1]);
+    console.log(myArray[2]);
+    console.log(myArray);
+    console.log("myArray",myArray);
 }//end setup
 
