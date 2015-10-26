bdb394998b2878fbcc8649a9b166b9702c687d58
Shuo Qiang <shuo.qiang@f1006-09-34181.bmcc.cuny.edu>
2015-10-26 15:51:46 -0400

    array with loop add bubble

--- MMP210-FA15-week7start/Week7-inClass-10.26.15-Start/js/main.js
+++ MMP210-FA15-week7start/Week7-inClass-10.26.15-Start/js/main.js
@@ -1,5 +1,16 @@
 //global variables
 
+//Bubble Constructor
+function Bubble(){
+    //create x and y properties(variables)
+    this.x = random(width);
+    this.y = random(height);
+    this.r = 20;//r for radius is 1/2 the width of the circle
+    //draw the bubble
+    this.bubbleSize = this.r*2;//width and height are twice the radius
+        ellipse(this.x,this.y,this.bubbleSize,this.bubbleSize);
+}//end Bubble
+
 function setup(){
     //create the canvas. put into a variable to associate with a element
     var cnv = createCanvas(800,3000);
@@ -8,22 +19,25 @@ function setup(){
     //test:var myArray = ["hello",3,"bye"];
     var myArray=[];//create blank array
 
+    for(var i=0;i<10;i++){
+    //add Bubbles to the array
+    myArray[i] = new Bubble();
+    }
+
     //add stuff to the array with a loop
     for(var i=0; i<1000;i++){
         //myArray[i] = "item"+i;
-        myArray[i]=i;
+        //myArray[i]=i;
+        myArray[i]= ellipse(i,i,10,10);
     }
+
     //loop to go through every item in the array
     for(var i=0;i<myArray.length;i++){
         myArray[i]+=10;
         //console.log(myArray[i]);
     }
+    // log out each item,for ellspses it will return the whole p5 object instead
         console.log(myArray);
 
-    console.log(myArray[0]);
-    console.log(myArray[1]);
-    console.log(myArray[2]);
-    console.log(myArray);
-    console.log("myArray",myArray);
 }//end setup
 
